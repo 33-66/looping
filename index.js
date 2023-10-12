263e69e268eb0956f255094fba493f93c73e2083
33-66 <simon.nganga@student.moringaschool.com>
2023-10-12 21:34:49 +0300

    updated work

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solutions in this file
+let cards=[];
+let names=['Guadalupe','Ollie','Aki'];
+let event = 'surprise';
+
+function writeCards(array,eventName){
+    for(let i=0; i < names.length;i++){
+       cards.push(`Thank you, ${array[i]}, for the wonderful ${eventName} gift!`)
+    }
+    return cards;
+}
+
+
+function countDown(num){
+    while(num>=0){
+        console.log(num)
+        num--;
+    }
+}
